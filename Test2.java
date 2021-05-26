372b07e5941b2d767d29984f107bd1d6925c16a0
worfran <fraworman@hotmail.com>
2021-05-25 23:46:25 -0500

    Primeras pruebas html

---
+++ Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2
+{
+    private int numero;
+
+    public Test2(int pnumero)
+    {
+        numero=pnumero;
+    }
+
+    public static void main(String[] args) 
+    {
+       System.out.println("Hola mundo"); 
+    }
+}
