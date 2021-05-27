68835774d8ec30964eaf250d22ac3bc15809a1e6
worfran <fraworman@hotmail.com>
2021-05-26 19:22:59 -0500

    Prueba de CSS

--- Test2.java
+++ Test2.java
@@ -1,13 +1,6 @@
 
 public class Test2
 {
-    private int numero;
-
-    public Test2(int pnumero)
-    {
-        numero=pnumero;
-    }
-
     public static void main(String[] args) 
     {
        System.out.println("Hola mundo"); 
